6cce174183764a89ddf2c3edc2b58071252b8853
john elisha <bahagoj@gmail.com>
2021-12-07 11:09:46 +0100

    #

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,16 +1,20 @@
+import { getLocaleNumberSymbol } from '@angular/common';
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { AlbumStorePage } from '../../e2e/app.po';
 
+import 'rxjs/add/operator/map';
 
   @Injectable()
   
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
-  constructor(private _http: Http) { 
-
+  constructor(private _http: Http) { }
+  getAlbum(id: Number) {
+    return  this._http.get(this._albumUrl).map((response)=> response.json());
   }
+
 
 }
 
