8474f754152387b5be9e02e967aade4db3ed54e1
john elisha <bahagoj@gmail.com>
2021-12-13 17:52:47 +0100

    #

--- src/app/product-page/product.service.ts
+++ src/app/product-page/product.service.ts
@@ -7,5 +7,10 @@ import { Http, Response } from '@angular/http';
 export class ProductService {
 
   constructor(private _http: Http) { }
-
+  getAlbum(id: Number):  Observable<Album> {
+    
+    return  this._http.get(this._albumUrl).map((response)=> <Album> response.json());}
+    getProducts(): Observable <Product[]> {
+      return this._http.get(this._productsUrl).map(Response => <Product[]>Response.json());
+    }
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -23,7 +23,7 @@ export class ProductService {
     return  this._http.get(this._albumUrl).map((response)=> <Album> response.json());}
     getProducts(): Observable <Product[]> {
       return this._http.get(this._productsUrl).map(Response => <Product[]>Response.json());
-    };
+    }
   }
 
 
